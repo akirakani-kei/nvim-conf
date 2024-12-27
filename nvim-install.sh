5fa08b71f93de30d780766f37337f14e0752233c
kei <52973114+akirakani-kei@users.noreply.github.com>
2024-12-27 21:47:48 +0200

    install script

--- nvim-install.sh
+++ nvim-install.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
-read -p "Running this script will erase your current neovim configuration, are you sure you want to continue? [Y/n]: " prompt
+if [ -z "$PROMPT" ]; then
+    read -p "Running this script will erase your current neovim configuration, are you sure you want to continue? [Y/n]: " prompt
+else
+    prompt="$PROMPT"
+fi
 
 prompt="${prompt:-Y}"
 prompt=$(echo "$prompt" | tr '[:upper:]' '[:lower:]')
 
 if [ "$prompt" = "y" ]; then
+    if [ -d "$HOME/.config/nvim" ]; then
+        rm -rf "$HOME/.config/nvim"
+    fi
 
-	if [ -d "$HOME/.config/nvim" ]; then
-            rm -rf "$HOME/.config/nvim"
-        fi
-
-        git clone https://github.com/akirakani-kei/nvim-conf
-        mv nvim-conf nvim
-        mv nvim ~/.config
-	rm ~/.config/nvim/autoload/plug.vim	# removes backup
-	wget https://raw.githubusercontent.com/junegunn/vim-plug/refs/heads/master/plug.vim -P ~/.config/nvim/autoload	# gets the latest version of vim-plug
-        rm ~/.config/nvim/nvim-install.sh	# removes redundant install script
+    git clone https://github.com/akirakani-kei/nvim-conf
+    mv nvim-conf nvim
+    mv nvim ~/.config
+    rm ~/.config/nvim/autoload/plug.vim  # removes backup
+    wget https://raw.githubusercontent.com/junegunn/vim-plug/refs/heads/master/plug.vim -P ~/.config/nvim/autoload  # gets the latest version of vim-plug
+    rm ~/.config/nvim/nvim-install.sh  # removes redundant install script
 
 else
-
-echo "aborting."
-exit 1
-
+    echo "aborting."
+    exit 1
 fi
 
 exit 0
