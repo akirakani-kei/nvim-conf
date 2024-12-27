d1890ea16d4df55438d9a7533296301f9da09eb1
kei <52973114+akirakani-kei@users.noreply.github.com>
2024-12-27 23:20:47 +0200

    Update nvim-install.sh

--- nvim-install.sh
+++ nvim-install.sh
@@ -14,6 +14,7 @@ if [ "$response" = "y" ] || [ "$response" = "yes" ]; then
 	rm ~/.config/nvim/autoload/plug.vim	# removes backup
 	wget https://raw.githubusercontent.com/junegunn/vim-plug/refs/heads/master/plug.vim -P ~/.config/nvim/autoload	#gets the latest version of vim-plug
         rm ~/.config/nvim/nvim-install.sh	# removes redundant install script
+	nvim +'PlugInstall --sync'
 
 	exit 0
 
